70cb64883b0d8ea75c5980102238691d051af499
jeremy-to <jeremy_to@outlook.fr>
2023-01-19 12:43:44 +0100

    new app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    'X-RapidAPI-Key': '7f5d99389dmshdc1132396327d4dp1870f6jsn9b55c8303ddf',
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "a44d34cbb7ae859ef8e4c92ddd5f3cb2"; 
